f9ffdd4922040785863a722a0792b2b3354b112d
bestbrandutilidades-cmyk <bestbrandutilidades@gmail.com>
2025-10-31 14:04:07 -0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-// ... (o resto do arquivo index.tsx continua exatamente igual)
+const root = document.getElementById("root");
+if (!root) throw new Error("Elemento #root não encontrado");
+
+createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
